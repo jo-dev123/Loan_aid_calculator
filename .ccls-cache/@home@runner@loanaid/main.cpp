f0a74cd39d19d2a48240ac263d33f1e1a6971e8f
Degbor Johnson Abel <64049960+jo-dev123@users.noreply.github.com>
2023-02-22 23:14:19 +0000

    ask user if they want to try again

--- .ccls-cache/@home@runner@loanaid/main.cpp
+++ .ccls-cache/@home@runner@loanaid/main.cpp
@@ -3,19 +3,24 @@ using namespace std;
 
 int main() {
   float face, interest, duration, needed;
-  cout << "How much do you need?" << endl;
-  cin >> needed;
-  cout << "What is the required interest rate?" << endl;
-  cin >> interest;
-  interest = interest / 100;
-  cout << "How many years will this loan last?" << endl;
-  cin >> duration;
-
-  face =  needed + (needed * interest * duration);
-  cout << endl << "===========================" << endl;
-  cout << endl << "Face-Value = " << face << endl;
-  cout << "===========================" << endl;
-  cout << endl << "Overall Interest charge = " << interest * duration << endl;
-  cout << "===========================" << endl;
-  cout << endl << "Payment each month = " << needed / (12 * duration);
+  for (string stopper; stopper != "No";) 
+  {
+    cout << "\n\n\nHow much do you need?" << endl;
+    cin >> needed;
+    cout << "What is the required interest rate?" << endl;
+    cin >> interest;
+    interest = interest / 100;
+    cout << "How many years will this loan last?" << endl;
+    cin >> duration;
+  
+    face =  needed + (needed * interest * duration);
+    cout << endl << "===========================" << endl;
+    cout << endl << "Face-Value = " << face << endl;
+    cout << "===========================" << endl;
+    cout << endl << "Overall Interest charge = " << interest * duration << endl;
+    cout << "===========================" << endl;
+    cout << endl << "Payment each month = " << needed / (12 * duration);
+    cout << "Do you want to calclate more values (Yes / No)" << endl;
+    cin >> stopper;
+  } 
 }
